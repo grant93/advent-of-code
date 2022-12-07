925d342b4f9b9a7f963c52fda8e6c4a82b7b2b6e
Brandon Grant <brandon.grant.93@gmail.com>
2022-12-08 00:27:31 +1100

    gofmt on day6

--- 2022/day6/solve.go
+++ 2022/day6/solve.go
@@ -7,33 +7,33 @@ import (
 )
 
 func findRepeats(data string) bool {
-	for i, char := range data[0:len(data)-1] {
+	for i, char := range data[0 : len(data)-1] {
 		for _, char2 := range data[i+1 : len(data)] {
 			if char == char2 {
-				return true 
+				return true
 			}
 		}
 	}
-	return false 
+	return false
 }
 
-func solve(data []string) []int{
+func solve(data []string) []int {
 	answers := make([]int, 2)
 	stream := data[0]
 
-	for i:= 0; i<len(stream); i++ {
+	for i := 0; i < len(stream); i++ {
 		if !findRepeats(stream[i : i+4]) {
-			if (answers[0] == 0) {
-				answers[0] = i+4
+			if answers[0] == 0 {
+				answers[0] = i + 4
 			}
 		}
 		if !findRepeats(stream[i : i+14]) {
-			answers[1] = i+14
+			answers[1] = i + 14
 		}
-		if (answers[0] != 0 && answers[1] != 0) {
+		if answers[0] != 0 && answers[1] != 0 {
 			break
 		}
-		
+
 	}
 
 	return answers
